ed24f008e2ba9dc83a717359607b93ed52781a9c
tanssive <tanssive@yahoo.com>
2020-10-07 17:31:10 +0200

    Delete shuffleCards.js

--- scripts/shuffleCards.js
+++
@@ -1,8 +0,0 @@
-export function shuffleCards() {
-    const cards = document.querySelectorAll('.card');
-
-    cards.forEach(card => {
-        let randomPos = Math.floor(Math.random() * cards.length);
-        card.style.order = randomPos;
-    });
-}
